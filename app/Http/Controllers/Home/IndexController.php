2f868eff4685f7d866c57144943e0a3658e3bb4c
raojiahong <2550621944@qq.com>
2019-07-01 13:59:38 +0800

    加载第三方扩展库

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers\Home;
 
+use extend\Pay\PayTest;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class IndexController extends Controller
 {
-    //
+    //调用第三方类库PayTest.php
     public function index(){
-        echo 'home/index';
+        $pay_obj = new PayTest();
+        $res = $pay_obj->index();
+        echo $res;
     }
 }
---
+++ extend/Pay/PayTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace extend\Pay;
+
+class PayTest
+{
+    public function index(){
+        return '进入了第三方支付类';
+    }
+}
